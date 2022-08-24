5e4ee6dbc32a2d8f7dd49adc44c01fc85c1a5a0b
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2022-08-24 17:05:53 -0500

    Completed live code.

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,38 @@
  * A functional and classical module import
  * example are provided.
  * */
-import { functionExample } from "./modules/function-example.js";
-import { ClassExample } from "./modules/class-example.js";
+// import { functionExample } from "./modules/function-example.js";
+// import { ClassExample } from "./modules/class-example.js";
 
-const title = document.querySelector("h1");
+// const title = document.querySelector("h1");
 
-if (title) {
-  functionExample(title);
-  new ClassExample(title);
+// if (title) {
+//   functionExample(title);
+//   new ClassExample(title);
+// }
+
+const tShirtForm = document.querySelector('.t-shirt-form');
+
+if (tShirtForm) {
+  tShirtForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nameEl = document.querySelector('#name').value;
+    const numberEl = document.querySelector('#count').value;
+    const sizesEl = document.querySelector('input[name="shirt-size"]:checked').value;
+    const shirtType = document.querySelector('#shirt-types');
+    const shirtTypeValue = shirtType.options[shirtType.selectedIndex].text;
+
+    // Set form values into message.
+    document.querySelector('.message').innerHTML = `<strong>Hello ${nameEl}!</strong> You want ${numberEl} ${sizesEl} ${shirtTypeValue}`;
+  });
 }
+
+
+// How I would handle the clear functionality.
+
+// Add listener for clear
+
+// Get elements to clear.
+
+// Do element.value = '' or similar;
